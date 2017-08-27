7683d06d306d03f1b31580f4a26c6422222d04f5
likole <likolef@gmail.com>
2017-08-27 15:28:24 +0800

    update Readme

--- Readme.md
+++ Readme.md
@@ -3,7 +3,9 @@
 ## 1.简单信息爬虫
 爬取成绩单页面简单的个人信息，无需密码
 ### 使用方法：
-```BasicInfo.start("asdfxcacscasdg");//JSESSIONID``` 
+先更改JSESSIONID
+
+```BasicInfo.start();``` 
 ## 2.详细信息爬虫
 爬取学籍信息中的详细信息，需要密码
 ### 使用方法
--- src/cn/likole/util/DBUtil.java
+++ src/cn/likole/util/DBUtil.java
@@ -3,6 +3,7 @@ package cn.likole.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
 
 /**
  * Created by likole on 8/22/17.
