d31c649760dfa8b6787dd374e18e9919e5c88a30
ChenFeng <likolef@gmail.com>
2017-10-17 20:01:59 +0800

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,46 @@
+create table JWXT.BASIC_INFO
+(
+	id char(10) not null,
+	name varchar(30) null,
+	gender tinyint(1) null,
+	idcard char(18) null,
+	nation varchar(30) null,
+	jg varchar(30) null,
+	zz varchar(30) null,
+	bj varchar(100) null,
+	zy varchar(100) null,
+	xs varchar(100) null
+)
+;
+
+create table JWXT.DETAIL_INFO_2017
+(
+	id char(10) not null,
+	name varchar(30) null,
+	gender tinyint(1) null,
+	idcard char(18) null,
+	nation varchar(30) null,
+	jg varchar(30) null,
+	zz varchar(30) null,
+	bj varchar(100) null,
+	zy varchar(100) null,
+	xs varchar(100) null,
+	kq varchar(30) null,
+	byzx varchar(100) null,
+	gkzf varchar(10) null,
+	lqh char(10) null,
+	gkksh varchar(20) null,
+	rxksyz char(10) null,
+	txdz varchar(200) null,
+	xq char(5) null,
+	wyyz char(10) null
+)
+;
+
+create table JWXT.ids
+(
+	id char(10) not null
+)
+;
+
+
